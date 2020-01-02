13048af85f3ccb90190e38ddaf12e1461ed9fdfd
sslash <michaelgunnulfsen@gmail.com>
2020-01-02 07:48:25 +0100

    initial commit

---
+++ GymBoxCell.swift
@@ -0,0 +1,13 @@
+//
+//  GymBoxCell.swift
+//  wodcrush
+//
+//  Created by michael gunnulfsen on 02/01/2020.
+//  Copyright © 2020 michael gunnulfsen. All rights reserved.
+//
+
+import UIKit
+
+class GymBoxCell: UITableViewCell {
+
+}
